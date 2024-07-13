aa73bb44aaea48b52f7d870347384ac72514deb0
Hyunwoo Choi <coladrop93@gmail.com>
2024-07-13 16:17:59 +0900

    [feat] datasheet component added

--- diaspora_eun/app/database/page.tsx
+++ diaspora_eun/app/database/page.tsx
@@ -1,3 +1,11 @@
+import styles from '../global.module.css';
+import Datasheet from './datasheet';
+
 export default function Page() {
-  return <h1>Database</h1>;
+  return (
+  <>
+    <h1 className={styles.title_texts}>Database</h1>
+    <Datasheet />
+  </>
+  );
 }
